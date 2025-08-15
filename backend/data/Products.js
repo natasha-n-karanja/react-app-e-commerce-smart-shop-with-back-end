2f699ca0f591c6aca1ca14aad6e218904405a300
natashankaranja <natashankaranja@gmail.com>
2025-08-15 10:31:14 +0300

    added data to backend

---
+++ backend/data/Products.js
@@ -0,0 +1,84 @@
+const products = [
+  {
+    id:1,
+    title:"Oraimo space buds",
+    price:4500,
+    image: "/src/images/wirelessheadphones.jpeg",
+    description: "Unique Design of Lunar Eclipse, Customize Your Voice Prompts, Personalized Lighting Effect"
+  },
+
+  {
+    id:2,
+    title:"Wireless Headphones",
+    price:2600,
+    image: "/src/images/headphones.webp",
+    description: "Powerful Deep Bass, Up to 60 Hours Long Playtime, ENC Technology"
+  },
+
+  {
+    id:3,
+    title:"Power Bank",
+    price:2700,
+    image: "/src/images/powerbank.webp",
+    description: "4 Built-in Charging & Recharging Cables, Wide Compatibility,Smart and Powerful"
+  },
+
+  {
+    id:4,
+    title:"Wireless Earbuds",
+    price:1900,
+    image: "/src/images/WirelessEarbuds.webp",
+    description: "Bluetooth Version: 5.3, Bluetooth Range: 10m, Playtime: Up to 7 hours on single charge; case provides additional 31 hours"
+  },
+
+  {
+    id:5,
+    title:"Wireless Speaker",
+    price:2500,
+    image: "/src/images/WirelessSpeaker.webp",
+    description: "Size: 146*72*50mm, Weight: 280g, Bluetooth version：V5.4"
+  },
+
+  {
+    id:6,
+    title:"Portable Blender",
+    price:3200,
+    image: "/src/images/PortableBlender.webp",
+    description: "Capacity: 500ML, Rated Power: 75W, Weight: 536.6g"
+  },
+
+  {
+    id:7,
+    title:"Electric Kettle",
+    price:3500,
+    image: "/src/images/Electric Kettle.webp",
+    description: "Material: PP+SUS304/T, Size: 1.7L, Power: 220V-240V/50Hz  1500W"
+  },
+
+  {
+    id:8,
+    title:"Wifi-connected Smart Plug",
+    price:1100,
+    image: "/src/images/Wifi-connected Smart Plug.webp",
+    description: "Plug Type: UK Type,Dimension: 50*58*52.8mm,Input: 30v·μ AC, 50/60Hz, 13A MAX"
+  },
+
+  {
+    id:9,
+    title:"Air Fryer",
+    price:14900,
+    image: "/src/images/Air Fryer.webp",
+    description: "Material: ABS+Cold Rolled Sheet, Rated Voltage: 220-240V 50/60Hz,Rated Power: 2000W"
+  },
+
+  {
+    id:10,
+    title:"Phone Case ",
+    price:1000,
+    image: "/src/images/Phone Case .webp",
+    description: "Firm Magnetic, Anti-slip, Support Wireless Charging , Screen , Camera Lens Protection"
+  },
+]
+
+
+export default products;
